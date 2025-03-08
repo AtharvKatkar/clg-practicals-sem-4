bae7b68f45721b8b18b80cd9ed9553c716e44ab3
DhanashriPokharkar <dhanashri08pokharkar@gmail.com>
2025-03-08 13:54:37 +0530

    Update btree19.h
    added search

--- btree19.h
+++ btree19.h
@@ -1,75 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
-{
-    int data;
-    struct Node *left;
-    struct Node *right;
+typedef struct Node {
+	int data;
+	struct Node *left;
+	struct Node *right;
 } Node;
 
-Node *createNode(int data)
-{
-    Node *nn = (Node *)malloc(sizeof(Node));
-    nn->data = data;
-    nn->left = NULL;
-    nn->right = NULL;
-    return nn;
+Node *createNode(int data){
+	Node *nn=(Node *)malloc(sizeof(Node));
+	nn->data=data;
+	nn->left=NULL;
+	nn->right=NULL;
+	return nn;
 }
 
-Node *createBST()
-{
-    int data;
-    printf("Enter data (Enter -1 to stop):\n");
-    scanf("%d", &data);
+Node* createBST(){
+	int data;
 
-    if (data == -1)
-    {
-        return NULL;
-    }
-
-    Node *root = createNode(data);
-    root->left = createBST();
-    root->right = createBST();
-    return root;
+	printf("Enter data (Enter -1 to exit):");
+	scanf("%d", &data);
+	
+	if(data == -1){
+		return NULL;
+	}
+	
+	Node *nn=createNode(data);
+	nn->left=createBST();
+	nn->right=createBST();
+	return nn;
 }
 
-int search(Node *root, int data)
-{
-    if (root == NULL)
-    {
-        return 0;
-    }
-
-    if (root->data = data)
-    {
-        return 1;
-    }
-
-    search(root->left, data);
-    search(root->right, data);
+void preorder(Node *root){
+	if(root==NULL){
+		return;
+	}
+	
+	printf("%d ", root->data);
+	preorder(root->left);
+	preorder(root->right);
 }
 
-void preorder(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-
-    printf("%d ", root->data);
-    preorder(root->left);
-    preorder(root->right);
-}
-
-void inorder(Node *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-
-    inorder(root->left);
-    printf("%d ", root->data);
-    inorder(root->right);
+void search(Node *root,int data){
+	if(root == NULL){
+		return;
+	}
+	
+	if(root->data==data){
+		printf("Key found in tree \n");
+	}
+	
+	search(root->left, data);
+	search(root->right, data);
 }
