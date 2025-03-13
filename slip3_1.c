c1e830cc1b4145f374a278b0a9e49d658fa12a66
AtharvKatkar <atharvkatkar47@gmail.com>
2025-03-13 21:31:09 +0530

    Added linear probing example

--- slip3_1.c
+++ slip3_1.c
@@ -6,24 +6,18 @@
 
 int table[SIZE];
 
-void insert(int data)
+void insert(int data, int org)
 {
-    int hash = data % SIZE;
+    int key = data % SIZE;
 
-    for (int i = 0; i < SIZE; i++)
+    if (table[key] == -1)
     {
-        if (table[i] == -1)
-        {
-            table[i] = data;
-        }
-        else
-        {
-            for (int j = i; j < SIZE; j++)
-            {
-                printf("Collision occured probing for place %d", j);
-                // Add code for linear probing
-            }
-        }
+        table[key] = org;
+    }
+    else
+    {
+        int newData = data + 1;
+        insert(newData, org);
     }
 }
 
@@ -42,5 +36,39 @@ void main()
     {
         printf("Enter element: ");
         scanf("%d", &data);
+        if (data == -1)
+        {
+            break;
+        }
+        else
+        {
+            insert(data, data);
+        }
+    }
+
+    printf("\nThe final hash table \n");
+    printf("|\tkey\t|\tvalue\t|\n");
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("|\t%d\t|\t%d\t|\n", i, table[i]);
     }
 }
+
+/*
+Expected output:
+Enter elements (-1 to stop)
+Enter element: 50
+Enter element: 70
+Enter element: 76
+Enter element: 85
+Enter element: 93
+Enter element: -1
+
+The final hash table
+|       key     |       value   |
+|       0       |       50      |
+|       1       |       70      |
+|       2       |       76      |
+|       3       |       85      |
+|       4       |       93      |
+*/
